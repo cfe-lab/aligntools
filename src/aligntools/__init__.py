656460665605d5466b0a021be65bf59ed1cb76e9
Vitaliy Mysak <vmysak@bccfe.ca>
2024-09-27 11:23:30 -0700

    Move Cigar class into its own file

--- src/aligntools/__init__.py
+++ src/aligntools/__init__.py
@@ -2,10 +2,11 @@
 from aligntools.libexceptions import CigarError, CoersionError, \
     ParseError, InvalidOperationError, CigarHitRangeError, \
     CigarConnectError, EmptyCigarHitListError
-from .lib import Cigar, CigarHit, connect_cigar_hits
+from .lib import CigarHit, connect_cigar_hits
 from .coordinate_mapping import CoordinateMapping
 from .int_dict import FrozenIntDict
 from .cigar_actions import CigarActions
+from .cigar import Cigar
 
 __all__ = [
     'CigarError',
